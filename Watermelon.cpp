08157f3b1851901791b4e548fc1cd980df822acb
Avnish Kumar <58879973+avnish0900@users.noreply.github.com>
2021-01-11 21:53:46 +0530

    Watermelon.cpp

---
+++ Watermelon.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,a;cin>>n;
+    a=n/2;
+      if(n%2==0 && a!=1)
+      {
+          cout<<"yes";
+      }
+      else
+      {
+          cout<<"no";
+      }
+  return 0;
+}
