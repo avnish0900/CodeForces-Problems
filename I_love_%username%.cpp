ce860de362b027513c4007d18e8ca6b9988de5e8
Avnish Kumar <58879973+avnish0900@users.noreply.github.com>
2021-01-27 20:08:01 +0530

    I_love_%username%.cpp

---
+++ I_love_%username%.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n,count=0;
+  cin>>n;
+  int a[n];
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+    }
+  int max=a[0],min=a[0];
+  for(int i=0;i<n;i++){
+    if(max<a[i]){
+      max=a[i];
+      ++count;
+    }
+    if(min>a[i]){
+      min=a[i];
+      ++count;
+    }
+  }
+
+  cout<<count;
+  return 0;
+}
