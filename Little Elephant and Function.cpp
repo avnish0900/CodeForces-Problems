159f1b8dc27c00e757687975fbe9a56480479ea8
Avnish Kumar <58879973+avnish0900@users.noreply.github.com>
2021-02-13 19:32:22 +0530

    Little Elephant and Function.cpp

---
+++ Little Elephant and Function.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int n;cin>>n;
+  if(n==1){cout<<"1";return 0;}
+  else if(n==2){cout<<"2 1";return 0;}
+  else{
+    cout<<n;
+  for(int i=1;i<n;i++){
+    cout<<" "<<i;
+  }}
+  return 0;
+}
