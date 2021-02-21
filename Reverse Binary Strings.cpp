cbeb95e2c73e58250def55bc09d405ceea303559
Avnish Kumar <58879973+avnish0900@users.noreply.github.com>
2021-02-21 11:24:27 +0530

    Reverse Binary Strings.cpp

---
+++ Reverse Binary Strings.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int t;cin>>t;
+  while(t--){
+    int n;cin>>n;
+    string s;
+    cin>>s;
+    int moves=0;
+    for(int i=0;i<n-1;i++){
+      if(s[i]==s[i+1]){
+        moves+=1;
+      }
+    }
+    cout<<ceil(moves/2.0)<<"\n";
+  }
+  return 0;
+}
