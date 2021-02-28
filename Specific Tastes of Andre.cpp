369460f0204beb9bd5272d20bc9c0d3e052e76e3
Avnish Kumar <58879973+avnish0900@users.noreply.github.com>
2021-02-28 16:58:18 +0530

    Specific Tastes of Andre.cpp

---
+++ Specific Tastes of Andre.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  long long t;cin>>t;
+  while(t--){
+    long long n;cin>>n;
+    for(long long i=0;i<n;i++){
+      cout<<2<<" ";
+    }
+    cout<<endl;
+  }
+}
