827531e4f75d40174617599a40d49dc258184922
Avnish Kumar <58879973+avnish0900@users.noreply.github.com>
2021-03-01 20:26:30 +0530

    Add Candies.cpp

---
+++ Add Candies.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  long long t;cin>>t;
+  while(t--){
+    long long n;cin>>n;
+    
+      cout<<n<<endl;
+      for(long long i=0;i<n;i++){
+        cout<<i+1<<" ";
+      }
+      cout<<endl;
+    
+  }
+  return 0;
+}
