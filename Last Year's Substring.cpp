b11bad2223e034ff06fe9325df743df6df7d3c88
Avnish Kumar <58879973+avnish0900@users.noreply.github.com>
2021-03-19 18:27:26 +0530

    Last Year's Substring.cpp

---
+++ Last Year's Substring.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int t,n,i;
+cin>>t;
+while(t--)
+{
+cin>>n;
+string s;
+cin>>s;
+if(s[0]=='2'&&s[n-1]=='0'&&s[n-2]=='2'&&s[n-3]=='0')
+{
+cout<<"YES"<<endl;
+}
+else if(s[0]=='2'&&s[1]=='0'&&s[n-1]=='0'&&s[n-2]=='2')
+{
+cout<<"YES"<<endl;
+}
+else if(s[0]=='2'&&s[1]=='0'&&s[2]=='2'&&s[3]=='0')
+{
+cout<<"YES"<<endl;
+}
+else if(s[n-1]=='0'&&s[n-2]=='2'&&s[n-3]=='0'&&s[n-4]=='2')
+{
+cout<<"YES"<<endl;
+}
+else if(s[0]=='2'&&s[1]=='0'&&s[2]=='2'&&s[n-1]=='0')
+{
+cout<<"YES"<<endl;
+}
+else
+cout<<"NO"<<endl;
+}
+}
