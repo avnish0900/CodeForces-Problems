99cf2a8e3bab1b67bd7d3a5c801c325850961f7b
Avnish Kumar <58879973+avnish0900@users.noreply.github.com>
2021-03-19 18:26:23 +0530

    Update Favorite Sequence.cpp

--- Favorite Sequence.cpp
+++ Favorite Sequence.cpp
@@ -1,1 +1,23 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  long long t;cin>>t;
+  while(t--){
+    long long n;cin>>n;
+    long long a[n];
+    for(long long i=0;i<n;i++){
+      cin>>a[i];
+    }
+    for(long long i=0;i<n;i++){
+      long long j=n-1-i;
+      if(i>j){
+        break;
+      }
+      if(i==j){
+        cout<<a[i]<<" ";
+      }else{
+      cout<<a[i]<<" "<<a[j]<<" ";}
+    }cout<<endl;
+  }
+}
