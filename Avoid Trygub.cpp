a59473b833a61ca89109d0b88821dfeae34b544e
Avnish Kumar <58879973+avnish0900@users.noreply.github.com>
2021-03-22 18:08:28 +0530

    Avoid Trygub.cpp

---
+++ Avoid Trygub.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  long long t;cin>>t;
+  while(t--){
+    long long n;cin>>n;
+    string s;cin>>s;
+    sort(s.begin(),s.end());
+    cout<<s<<endl;
+  }
+}
