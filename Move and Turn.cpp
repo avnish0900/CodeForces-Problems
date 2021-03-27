6eefb486716f895322c0131835d9e11f72d392ab
Avnish Kumar <58879973+avnish0900@users.noreply.github.com>
2021-03-27 09:28:15 +0530

    Move and Turn.cpp

---
+++ Move and Turn.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  long long n;cin>>n;
+  if(n%2){
+    cout<<2*(n/2 +1)*(n/2 +2)<<endl;
+  }
+  else{
+    cout<<(n/2+1 )*(n/2+1)<<endl;
+  }
+}
