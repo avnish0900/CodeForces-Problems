abe6a31510ca4e338a71b24dc4a562f0c8685623
Avnish Kumar <58879973+avnish0900@users.noreply.github.com>
2021-05-17 09:09:26 +0530

    Potion-making.cpp

---
+++ Potion-making.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int              long long int
+#define all(xs)          xs.begin(), xs.end()
+#define rall(xs)         xs.rbegin(), xs.rend()
+#define sz(x)            (int)((x).size())
+#define pb               push_back
+#define mp               make_pair
+#define prec(n)          fixed<<setprecision(n)
+#define debug(x)         cout << (#x) << " is " << (x) << "\n"
+#define debug2(x, y)     cout << #x << "=" << x << "," << #y << "=" << y << "\n"
+#define debug3(x, y,z)   cout << #x << "=" << x << "," << #y << "=" << y << "," << #z << "=" << z << "\n"
+#define yes              cout<<"YES";
+#define no               cout<<"NO";
+#define ll               cout<<"\n";
+#define nil              0
+#define inf              1e15
+#define SZ               200005
+int gcd(int A,int B){    return (B == 0) ? A : gcd(B, A % B);}
+int lcm(int A,int B){    return A * B / gcd(A, B);}
+priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+const int N = 1e6 + 11;
+
+
+
+void solve(){
+  int n;cin>>n;
+  int common = gcd(n,100-n);
+  int moves=100/common;
+  cout<<moves;ll;
+}
+signed main(){
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  #endif
+  ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+  int t=1;
+  cin>>t;
+  while(t--){
+    solve();                                                    
+  }
+}
