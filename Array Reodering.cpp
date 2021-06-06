eb650e863b9d003b7e2d3688023e91cac8b6239d
Avnish Kumar <58879973+avnish0900@users.noreply.github.com>
2021-06-06 07:48:09 +0530

    Array Reodering.cpp

---
+++ Array Reodering.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int              long long int
+#define all(xs)          xs.begin(), xs.end()
+#define rall(xs)         xs.rbegin(), xs.rend()
+#define sz(x)            (int)((x).size())
+#define pb               push_back
+#define ppb              pop_back
+#define pf               push_front
+#define ppf              pop_front
+#define mp               make_pair
+#define ff               first
+#define ss               second
+#define prec(n)          fixed<<setprecision(n)
+#define debug(x)         cout << (#x) << " is " << (x) << "\n"
+#define debug2(x, y)     cout << #x << "=" << x << "," << #y << "=" << y << "\n"
+#define debug3(x, y,z)   cout << #x << "=" << x << "," << #y << "=" << y << "," << #z << "=" << z << "\n"
+#define yes              cout<<"YES";
+#define no               cout<<"NO";
+#define ll               cout<<"\n";
+#define nil              0
+#define inf              1e15
+#define inf18            1e18
+#define SZ               200005
+int gcd(int A,int B){    return (B == 0) ? A : gcd(B, A % B);}
+int lcm(int A,int B){    return A * B / gcd(A, B);}
+priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+const int nn=1e5+10;
+    
+void solve(){
+    int n;cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    // debug(gcd(5,5));
+    // sort(all(a));
+    int ans=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(gcd(2*a[i],a[j])>1   or gcd(a[i],2*a[j])>1){
+                ans+=1;
+            }
+        }
+    }
+    cout<<ans;ll;
+    return;
+}
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int t=1;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        // cout<<"Case #"<<i+1<<": "; 
+        solve();                                                    
+    }
+}
