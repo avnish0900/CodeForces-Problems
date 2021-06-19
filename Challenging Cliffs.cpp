f9468f9e38517e20ebb6f47d25086719328706d0
Avnish Kumar <58879973+avnish0900@users.noreply.github.com>
2021-06-19 07:48:36 +0530

    Challenging Cliffs.cpp

---
+++ Challenging Cliffs.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int              long long int
+#define all(xs)          xs.begin(), xs.end()
+#define rall(xs)         xs.rbegin(), xs.rend()
+#define sz(x)            (int)((x).size())
+#define pb               push_back
+#define ppb              pop_back
+#define pf               push_front
+#define ppf              pop_front
+#define mp               make_pair
+#define ff               first
+#define ss               second
+#define prec(n)          fixed<<setprecision(n)
+#define debug(x)         cout << (#x) << " is " << (x) << "\n"
+#define debug2(x, y)     cout << #x << "=" << x << "," << #y << "=" << y << "\n"
+#define debug3(x, y,z)   cout << #x << "=" << x << "," << #y << "=" << y << "," << #z << "=" << z << "\n"
+#define yes              cout<<"YES";
+#define no               cout<<"NO";
+#define ll               cout<<"\n";
+#define nil              0
+#define inf              1e15
+#define inf18            1e18
+#define SZ               200005
+int gcd(int A,int B){    return (B == 0) ? A : gcd(B, A % B);}
+int lcm(int A,int B){    return A * B / gcd(A, B);}
+priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+const int nn=1e5+10;
+
+void solve(){
+    int n;cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    int mi = INT_MAX;
+    int xx=0,yy=0;
+    for(int i=1;i<n;i++){
+        if(a[i]-a[i-1] < mi){
+            mi =a[i]-a[i-1];
+            xx = i,yy=i-1;
+        }
+    }
+    // debug2(xx,yy);
+    // debug2(a[xx],a[yy]);
+    cout<<a[yy]<<" ";
+    for(int i=yy+1;i<n;i++){
+        if(i==xx){
+            continue;
+        }
+        cout<<a[i]<< " ";
+    }
+    for(int i=0;i<=yy;i++){
+        if(i==yy){
+            continue;
+        }
+        cout<<a[i]<<" ";
+    }
+    cout<<a[xx];
+    ll;
+}
+
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int t=1;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        // cout<<"Case #"<<i+1<<": "; 
+        solve();                                                    
+    }
+}
